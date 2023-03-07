34f91bd7fe15f5375a0215bffd35659ce240f6cf
Bribri18 <122388718+Townsend777@users.noreply.github.com>
2023-03-07 09:59:42 -0800

    Yes
    Yes

--- Unit2TownsendP2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2TownsendP2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerbound = -10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        if (transform.position.z > topBound)  
+        { 
+            Destroy(gameObject);  
+        }  
+        else if (transform.position.z < lowerbound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
